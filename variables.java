13cd501c4b8279730317a7b5f5235271b73b32f8
NIKESH <nikeshnoida@gmail.com>
2025-02-27 21:13:03 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,17 @@
+//this is a comment
+public class variables {
+    public static void main(String[] args) {
+        String a = "java";
+        int num =108;
+        num = 103;
+        final String b = "java ee";
+//        b = "java"
+        System.out.println("here is "+ a + " "+  num + b);
+
+        int a1=55;
+        float a2=22.5f;
+        double a3=23.51654d;
+        double a4 = a1+a2+a3;
+        System.out.printf("here is addition "+a1+a2+a3+" "+a4);
+    }
+}
