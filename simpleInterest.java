99cb2aaf07341c054420919f30ee6dfd2ad35eab
NIKESH <nikeshnoida@gmail.com>
2025-02-27 21:14:06 +0530

    Add files via upload

---
+++ simpleInterest.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class simpleInterest{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Enter the principle:");
+        float ab = input.nextFloat();
+        System.out.printf("Enter the rate:");
+        float ac = input.nextFloat();
+        System.out.printf("Enter the time in years:");
+        float aa = input.nextFloat();
+        float ad = (aa*ab*ac)/100;
+        System.out.printf("the amount to pay is:"+" "+ad);
+    }
+}
