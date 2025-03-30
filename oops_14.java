f511830b19060b449c495305b9632a2f3cc4c4dc
NIKESH <nikeshnoida@gmail.com>
2025-03-30 23:26:25 +0530

    oops_14.java

--- oops_14.java
+++ oops_14.java
@@ -1,3 +1,4 @@
+//this example demonstrate constructor chaining that how does it execute 
 package oops;
 class laptop{
     String brand;
@@ -18,8 +19,8 @@ class laptop{
 
 public class oops_14 {
     public static void main(String[] args) {
-        laptop obj1 = new laptop("hp");
-        laptop obj2 = new laptop("lenovo",512);
+        laptop obj1 = new laptop("hp");                           //invoke constructor1 
+        laptop obj2 = new laptop("lenovo",512);                   //invoke constructor2 which chained with constuctor1 
 
         System.out.println("brand is  "+obj1.brand +"  ram is  "+ obj1.ram);
         System.out.println("brand is  "+obj2.brand +"  ram is  "+ obj2.ram);
