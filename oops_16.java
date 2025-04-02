94f2fc7c47219ebf6716118e2f304ecaa1d071fa
NIKESH <nikeshnoida@gmail.com>
2025-04-02 21:08:14 +0530

    Add files via upload

---
+++ oops_16.java
@@ -0,0 +1,29 @@
+package oops;
+
+public class oops_16 {
+    public static void main(String[] args) {
+        // Autoboxing
+        Integer num = 10;
+
+        // Unboxing and toString
+        String numStr = num.toString();
+        System.out.println("String: " + numStr);
+
+        // Parsing
+        int parsed = Integer.parseInt("20");
+        System.out.println("Parsed: " + parsed);
+
+        // Comparison
+        Integer num2 = 15;
+        int compare = num.compareTo(num2);
+        System.out.println("Compare: " + compare);
+
+        // Max/Min
+        System.out.println("Max: " + Integer.MAX_VALUE);
+        System.out.println("Min: " + Integer.MIN_VALUE);
+
+        //equals
+        Integer num3 = 10;
+        System.out.println("Equals: " + num.equals(num3));
+    }
+}
