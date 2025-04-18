64b2d65e4514713af7a499138bb2834d120ce4cc
NIKESH <nikeshnoida@gmail.com>
2025-04-18 21:45:57 +0530

    oops_17.java

--- oops_17.java
+++ oops_17.java
@@ -3,12 +3,12 @@ package oops;
 
 public class oops_17 {
     void mett() {
-        System.out.println("this is outer class method");
+        System.out.println("this is outer class method");  //this is outer class method
     }
-        private String out = "this is outer class private attribute";
+        private String out = "this is outer class private attribute"; 
     class inner{
-        void met(){
-            System.out.println("this is inner");
+        void met(){    //this is inner class method which have access of outer class attributes
+            System.out.println("this is inner");  
             System.out.println(out);
         }
     }
