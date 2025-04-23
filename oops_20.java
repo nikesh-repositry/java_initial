494e66dda6719a078601104b1c1228fbe7587320
NIKESH <nikeshnoida@gmail.com>
2025-04-23 14:19:36 +0530

    Add files via upload

---
+++ oops_20.java
@@ -0,0 +1,35 @@
+package oops;
+class parent{
+    public parent(){
+        System.out.println("parent no-args constructor executed");
+        parentMethod1();
+    }
+    public parent(String message){
+        System.out.println("parent string constructor executed");
+        parentMethod2(message);
+    }
+    public void parentMethod1(){
+        System.out.println("parent method1 executed");
+    }
+    public void parentMethod2(String message){
+        System.out.println("parent method2 executed with message "+" "+message);
+    }
+}
+class child extends parent{
+    public child(){
+        super();
+        System.out.println("child constructor with no argument executed");
+    }
+    public child(String message){
+        super(message);
+        System.out.println("child class string constructor executed with message"+" "+message);
+    }
+}
+
+public class oops_20 {
+    public static void main(String[] args) {
+        child obj1 = new child();
+        System.out.println("--------------------------------");
+        child obj2 = new child("HELLO THERE");
+    }
+}
