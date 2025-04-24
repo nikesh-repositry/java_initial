0b0f55b0a3f9af1e045686a066562e75da67c334
NIKESH <nikeshnoida@gmail.com>
2025-04-24 20:22:30 +0530

    Add files via upload

---
+++ oops_21.java
@@ -0,0 +1,52 @@
+package oops;
+class  A {
+    String name;
+    A (String name){
+        this.name=name;
+    }
+    void instanceMethod(){
+        System.out.println(name+"parent instance method");
+    }
+    static void staicMethod(){
+        System.out.println("parent static method");
+    }
+    final void finalMethod(){
+        System.out.println(name+"parent final method");
+    }
+}
+class B extends A{
+    B(String name){
+        super(name);
+    }
+
+    @Override
+    void instanceMethod() {
+        super.instanceMethod();
+        System.out.println(name+"child instance method");
+    }
+    static void staticMethod(){
+        System.out.println("static child class method");
+    }
+}
+public class oops_21 {
+    public static void main(String[] args) {
+        A obj1 = new A("this is ");
+        A obj2 = new B("THIS is ");
+        B obj3 = new B("THAT is ");
+
+        System.out.println("method overriding-------");
+        obj1.instanceMethod();
+        obj2.instanceMethod();
+        obj3.instanceMethod();
+
+        System.out.println("final method-----------");
+        obj1.finalMethod();
+        obj2.finalMethod();
+        obj3.finalMethod();
+
+        System.out.println("static method hiding--------");
+        A.staicMethod();
+        B.staticMethod();
+
+    }
+}
