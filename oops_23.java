e5ade8c7ee2c3d14e9295f498a990dd255a8b59f
NIKESH <nikeshnoida@gmail.com>
2025-04-30 22:18:49 +0530

    Add files via upload

---
+++ oops_23.java
@@ -0,0 +1,21 @@
+package oops;
+//abstract class ClassName {
+//    // Fields
+//    private type fieldName;
+//
+//    // Constructor
+//    public ClassName(type parameter) {
+//        this.fieldName = parameter;
+//    }
+//
+//    // Abstract method
+//    abstract returnType methodName();
+//
+//    // Concrete method
+//    public returnType concreteMethod() {
+//        // Implementation
+//        return value;
+//    }
+//}
+public class oops_23 {
+}
